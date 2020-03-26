f11246941c179a4a9c24f6f4afcdfa64ef0ebbe1
Jim Anderson <jima.coding@gmail.com>
2020-03-25 20:35:21 -0600

    First stand alone functional program.  Still could use some cleaning

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # RPstats
-Tool for processing readership stats from Real Python
+Tool for processing readership stats from Real Python.
+
+
+
+To run, pull down the repo, install the requirements.txt file with pip, then use:
+
+```bash
+$ ./analyze_message.py <input_file>
+```
+
+The input file is just the raw text copied from the slackbot reports.  NOTE: I've not extensively tested this.  I suspect it will act goofy if you don't start the file at the top of one of the messages.
+
+
+
--- analyze_message.py
+++ analyze_message.py
@@ -1,18 +1,20 @@
-#!/usr/bin/env python3.7
-"""Analyze a message, store to csv"""
+#!/usr/bin/env python3
+"""Analyze Raw text from real python team slack and produce a graph. """
 
-import pathlib
-import sys
-
+import argparse
 from bokeh.layouts import row
 from bokeh.models import ColumnDataSource
 from bokeh.models.tools import HoverTool
 from bokeh.models.widgets import CheckboxGroup
 from bokeh.plotting import figure, curdoc
+from bokeh.server.server import Server
 from dateutil.parser import parse as date_parse
 import numpy as np
 import pandas as pd
 import parse
+import pathlib
+import sys
+
 
 PATTERNS = dict(
     date=parse.compile("Between {_} and {date}, your articles"),
@@ -56,27 +58,20 @@ def create_figure(titles, sources):
 
     hover = HoverTool()
     hover.tooltips=[
-        ('time', '@date{%F-%T}'),
-        ('rss', '@info'),
+        ('date', '@date{%F-%T}'),
+        ('readers', '@info'),
     ]
     hover.formatters = {
         'date'      : 'datetime', # use 'datetime' formatter for 'date' field
     }
-    hover.mode = "vline"
+    # hover.mode = "vline"
     p.add_tools(hover)
     return p
 
 
-TITLES = []
-ORIG_LINES = []
-SRCS = []
-
 def callback(selections):
-    print("JIMA CALLBACK")
-    print("JIMA SRCS", callback.srcs)
     new_src = []
     new_titles = []
-    # LINES.clear()
     for item in selections:
         new_src.append(callback.srcs[item])
         new_titles.append(callback.titles[item])
@@ -115,64 +110,76 @@ def analyze(message):
             data[slug]["users"].append(result["users"])
             data[slug]["view_time"].append(result["time"])
 
-    return data
-
-
-def old_analyze(message):
-    lines = message.split("\n")
-    date = None
-    data = list()
-
-    for line in lines:
-        date_search = PATTERNS["date"].search(line)
-        if date_search:
-            date = date_parse(date_search.named["date"])
-
-        data_search = PATTERNS["data"].search(line)
-        if data_search:
-            result = data_search.named
-            mins, _, secs = result.pop("time").partition(":")
-            result["time"] = int(secs) + int(mins) * 60
-            if date:
-                result["date"] = date
-            if "github-intro" in result["slug"]:
-                data.append(result)
-
-    return data
-
-def main():
-    path = pathlib.Path(sys.argv[1])
-    print(f"Working on {path}")
-    data = analyze(path.read_text())
-
     srcs = []
-    for item, value in data.items():
-        # print(item, value)
+    for value in data.values():
         dates = np.array(value['date'], dtype=np.datetime64)
         views = np.array(value['views'])
         srcs.append(ColumnDataSource(data=dict(date=dates, info=views)))
 
+    return srcs, list(data.keys())
+
+
+def graph_it(doc):
+    path = pathlib.Path(sys.argv[1])
+    srcs, titles = analyze(path.read_text())
 
     callback.srcs = srcs
-    titles = list(data.keys())
     callback.titles = titles
+
     p = create_figure(titles, srcs)
-    # curdoc().add_root(p)
+    # doc.add_root(p)
 
     actives = [x for x in range(len(srcs))]
     callback.checkbox_group = CheckboxGroup(labels=titles, active=actives)
     callback.checkbox_group.on_click(callback)
-    curdoc().add_root(row(callback.checkbox_group, p))
+    doc.add_root(row(callback.checkbox_group, p))
 
-    # curdoc().clear()
-    # curdoc().add_root(row(checkbox_group, p))
-
-
-    # output_path = path.with_suffix(".csv")
-    # print(f"Writing output to {output_path}")
-    # pd.DataFrame.from_dict(data).to_csv(output_path)
-
+def get_command_line_args():
+    """ Read command line args, of course."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--no-checkbox", action="store_true")
+    parser.add_argument("input_file", help="raw text copied from slack")
+    return parser.parse_args()
 
 
-main()
+def start_server(address, port, url, attempts=100):
+    while attempts:
+        attempts -= 1
+        try:
+            server = Server(
+                {url: graph_it},
+                num_procs=1,
+                port=port,
+                address=address,
+                allow_websocket_origin=[f"{address}:{port}",],
+            )
+            server.start()
+            return server, port
+        except OSError as ex:
+            if "Address already in use" in str(ex):
+                print(f"Port {port} busy")
+                port += 1
+            else:
+                raise ex
+    raise Exception("Failed to find available port")
 
+
+if __name__ == "__main__":
+    args = get_command_line_args()
+    # These can be added as command line args if you want to move them around
+    port = 5006
+    address = "localhost"
+    url = "/"
+
+    try:
+        server, port = start_server(address, port, url)
+    except Exception as ex:
+        print("Failed:", ex)
+        sys.exit()
+
+    try:
+        print(f"Opening Bokeh application on http://{address}:{port}{url}")
+        server.io_loop.add_callback(server.show, url)
+        server.io_loop.start()
+    except KeyboardInterrupt:
+        print("\nShutting Down")
